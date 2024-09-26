4368a4352fd6d162a60cafc5c9ba2fae1eeeaa96
febrianpaper <febriansyah.rayiid@paper.id>
2024-09-26 14:28:39 +0700

    add config and chat

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/febriansyahnr/go-ai/config"
+	openai "github.com/sashabaranov/go-openai"
+)
 
 func main() {
-	fmt.Println("AI")
+	_, secret, err := config.LoadConfig("./.config.yaml", "./.secret.yaml")
+	if err != nil {
+		log.Fatalf("Error Loading config files: %v", err)
+		return
+	}
+	client := openai.NewClient(secret.ChatGPTToken)
+
+	messages := make([]openai.ChatCompletionMessage, 0)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Conversation")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: text,
+		})
+
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:    openai.GPT4o,
+				Messages: messages,
+			},
+		)
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			continue
+		}
+
+		content := resp.Choices[0].Message.Content
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: content,
+		})
+		fmt.Println(content)
+	}
 }
